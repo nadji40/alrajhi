ff2daabdbf9537e5dfa68b0c37ac7097eb6f5f42
nadji40 <51221840+nadji40@users.noreply.github.com>
2025-10-25 15:23:21 +0100

    ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,16 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-07-11',
 
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true,
+      autoSubfolderIndex: false
+    }
+  },
+
 
   eslint: {
     config: {
